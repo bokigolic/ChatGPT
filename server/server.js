be3b5c4b30cefbf747e7a3657f0072644d408f87
bokigolic <bokigolic32@gmail.com>
2023-02-26 12:23:56 -0500

    all ready for get response from AI

--- server/server.js
+++ server/server.js
@@ -28,15 +28,19 @@ app.post('/', async (req, res) => {
 
       model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0.7,
-      max_tokens: 64,
+      temperature: 0,
+      max_tokens: 3000,
       top_p: 1,
-      frequency_penalty: 0,
+      frequency_penalty: 0.5,
       presence_penalty: 0,
-      stop: ["\"\"\""],
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text
     })
 
   } catch (error) {
-
+    console.log(error);
+    res.status(500).send({ error })
   }
 });

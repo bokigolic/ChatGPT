43309b37245991e54126eccb17a10bcf46ac9f0e
bokigolic <bokigolic32@gmail.com>
2023-02-25 22:59:49 -0500

    install dependencies, prepare server

--- client/script.js
+++ client/script.js
@@ -1,38 +1,38 @@
 import bot from './assets/bot.svg'
 import user from './assets/user.svg'
 
-const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_container');
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
 
-let loadInterval;
-
+let loadInterval
 
 function loader(element) {
-  element.textContent = '';
+    element.textContent = ''
 
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += '.';
 
-  loadInterval = setInterval(() => {
-    element.textContent += '.';
-
-    if (element.textContent === '....') {
-      element.textContent = '';
-    }
-  }, 300)
-
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
 }
 
 function typeText(element, text) {
-  let index = 0;
+    let index = 0
 
-  let interval = setInterval(() => {
-    if (index < text.length) {
-      element.innerHTML += text.chartAt(index)
-      index++
-    } else {
-      clearInterval(interval)
-    }
-  }, 20)
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
 }
+
 
 
 function generateUnuqueId() {
